36a38efac487f72a51422de1ad4db4ef0a90a89e
solayap2601 <solayap@unal.edu.co>
2022-08-28 12:38:48 -0500

    Update tv.py

--- televisores/tv.py
+++ televisores/tv.py
@@ -51,19 +51,19 @@ class TV:
   
   def canalUp(self):
     if self._canal<120 and self._canal>0 and self._estado== True:
-      self._canal += 1
+      self._canal = self._canal + 1
   
   def canalDown(self):
     if self._canal<121 and self._canal>1 and self._estado== True:
-      self._canal -= 1
+      self._canal = self._canal - 1
 
   def volumenUp(self):
     if self._volumen<7 and self._volumen>=0 and self._estado== True:
-      self._volumen += 1
+      self._volumen = self._volumen + 1
 
   def volumenDown(self):
     if self._volumen<8 and self._volumen>0 and self._estado== True:
-      self._volumen -= 1
+      self._volumen = self._volumen -1
 
   @classmethod
   def getNumTV(cls):
